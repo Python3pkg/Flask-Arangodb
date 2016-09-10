f9c13d6a0eebefa892fd36ec2c00ab8a2be21c26
Bas van den Broek <cwasvandenbroek@gmail.com>
2016-09-10 23:05:06 +0200

    update deployment script

--- flask_arangodb/arangodb.py
+++ flask_arangodb/arangodb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import arango
+from arango import ArangoClient
 from flask import current_app
 
 try:
@@ -16,23 +16,29 @@ class ArangoDB(object):
             self.init_app(app)
 
     def init_app(self, app):
-        self.app = app
-        app.config.setdefault('ARANGO_SETTINGS', {
-            "host": "127.0.0.1",
-            "port": 8529,
-            "username": "root",
-            "password": ""
-        })
-        app.config.setdefault('ARANGO_DB', "default")
+        app.config.setdefault('ARANGO_CLIENT', {"host": "127.0.0.1",
+                                                "port": 8529,
+                                                "username": "root",
+                                                "password": ""})
+        app.config.setdefault('ARANGO_DB', {'name': 'default',
+                                            'user': None,
+                                            'password': None})
+
+    def connect(self):
+        return ArangoClient(**current_app.config['ARANGO_CLIENT'])
 
     @property
     def client(self):
         ctx = stack.top
         if ctx is not None:
-            if not hasattr(ctx, 'arangoclient'):
-                ctx.arangoclient = arango.ArangoClient(**current_app.config['ARANGO_SETTINGS'])
-            return ctx.arangoclient
+            if not hasattr(ctx, 'arango_client'):
+                ctx.arango_client = self.connect()
+            return ctx.arango_client
 
     @property
     def db(self):
-        return self.client.db(name=self.app.config['ARANGO_DB'])
+        if self.client:
+            if isinstance(current_app.config['ARANGO_DB'], dict):
+                return self.client.db(**current_app.config['ARANGO_DB'])
+            else:
+                return self.client.db(name=current_app.config['ARANGO_DB'])
